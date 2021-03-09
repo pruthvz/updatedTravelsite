e932990e47fdb89d651926a7e812a47105e5b9e7
pruthvi10 <pruthvimohanlal10@gmail.com>
2021-03-09 23:33:01 +0000

    quick updates

--- src/js/locationSlider.js
+++ src/js/locationSlider.js
@@ -2,7 +2,7 @@ const locationData = [
     {
       id: 1,
       pic:
-        "https://images.unsplash.com/photo-1571903300545-c165be5669f8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/SabarmatiThermalPlant.jpg/1280px-SabarmatiThermalPlant.jpg",
       name: "Ahmedabad",
       price: 423,
       offer: false,
